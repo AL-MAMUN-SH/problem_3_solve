774f1aabe257b8b441848589507d1b30bf577085
AL-MAMUN <almamoon604@gmail.com>
2021-08-27 00:56:20 +0600

    Added mark & AVerage & Compare Done

--- script.js
+++ script.js
@@ -18,4 +18,44 @@ var john = {
      }
 }
 john.calcTips();
-console.log(john)
+
+//  AVerage Tips CalCulate KE koto tk tips er beshi diboh
+
+var mark = {
+     fulname : 'Mark Miller',
+     bills : [77,475,110,45],
+     calcTips : function(){
+          this.tips = []
+          this.tipsWithBills = []
+      for(var i=0;i<this.bills.length;i++){
+               var parcentage
+               bill = this.bills[i]
+
+               if(bill<100){parcentage = .2}
+               else if(bill>=100 && bill<300){parcentage = .1}
+               else{parcentage = .25}
+
+               this.tips[i]= bill*parcentage
+               this.tipsWithBills[i]= bill + (bill*parcentage)
+          }
+     }
+}
+mark.calcTips()
+console.log(john,mark)
+// AVERAGE VALU IS HERE
+
+function calcAverage(tips){
+     var sum = 0
+     for(var i = 0; i<tips.length;i++){
+           sum = sum + tips[i]
+     }
+     return sum/tips.length
+}
+john.averageTips = calcAverage(john.tips)
+mark.averageTips = calcAverage(mark.tips)
+console.log(john,mark)
+
+// COmapare With 2 Family who only Big Tipper
+if(john.averageTips>mark.averageTips){console.log(john.fulname + '\'S Higher Tips,With an Average $ ' + john.averageTips)}
+else if(mark.averageTips>john.averageTips){console.log(mark.fulname +  '\'S Higher Tips,With an Average $' + mark.averageTips)}
+// console.log(john,mark)
